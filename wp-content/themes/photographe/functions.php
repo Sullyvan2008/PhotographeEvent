c5485470565167329c9dd244a665fd028e318de1
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 18:27:01 +0200

    re correction

--- wp-content/themes/photographe/functions.php
+++ wp-content/themes/photographe/functions.php
@@ -28,7 +28,7 @@ function photographe_enqueue_scripts() {
     wp_enqueue_style( 'photographe-style', get_stylesheet_uri() );
     
     // Enqueue un script personnalisé (exemple)
-    wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/custom.js', array(), '1.0', true );
+    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . 'js/custom.js');
 }
 add_action( 'wp_enqueue_scripts', 'photographe_enqueue_scripts' );
 
