f5f42fca1f243dc155f129e78a72d493ccd5a251
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 21:50:17 +0200

    fonts

--- wp-content/themes/photographe/template-parts/contact.php
+++ wp-content/themes/photographe/template-parts/contact.php
@@ -1,7 +1,11 @@
 <div id="myModal" class="modal">
   <div class="modal-content">
     <span class="close">&times;</span>
-    <h2>Contactez-nous</h2>
+    <div class="title-modale">
+    <img class="title-contact"
+     src="http://localhost:8888/PhotographeEvent/wp-content/uploads/2023/06/Group-23.png"
+     alt="Titre contact">
+    </div>
     <div class="contact-form">
       <?php echo do_shortcode('[contact-form-7 id="26" title="Contact form 1"]'); ?>
     </div>
