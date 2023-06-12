9657845156a88463fefdfd6388226ff213683aca
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 16:18:45 +0200

    header

--- wp-content/themes/photographe/header.php
+++ wp-content/themes/photographe/header.php
@@ -1,3 +1,9 @@
-<?php wp_nav_menu([
-    'theme_location' => 'header'
-]) ?>
+<?php
+  // Afficher le menu principal
+  wp_nav_menu( array(
+    'theme_location' => 'primary-menu',
+    'container' => 'nav',
+    'container_class' => 'main-menu-container',
+    'menu_class' => 'main-menu',
+  ) );
+?>
