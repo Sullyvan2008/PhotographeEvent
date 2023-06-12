6c6f9593c601fefdd597b369e46f29f1fca31ada
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 16:01:46 +0200

    functions.php

---
+++ wp-content/themes/photographe/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function photographe_supports()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+}
+
+add_action('after_setuptheme', 'photographe_supports');
