3e3a2a7cca0df07a721d218603397a57ceddac26
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 18:25:05 +0200

    correction functions.php

--- wp-content/themes/photographe/js/custom.js
+++ wp-content/themes/photographe/js/custom.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('myModal');
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("close")[0];
-  
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-  
-    span.onclick = function() {
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+
+  // Au clic sur le lien "Contact", ouvrir la modale
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+  });
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
-  
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-  });
-  
+  }
+});
