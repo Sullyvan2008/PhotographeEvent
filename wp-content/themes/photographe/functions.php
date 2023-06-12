3076dff74fff124b9af974c9ac5cfc747ca585df
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 17:15:27 +0200

    update

--- wp-content/themes/photographe/functions.php
+++ wp-content/themes/photographe/functions.php
@@ -1,18 +1,25 @@
 <?php
-
-function photographe_supports()
-{
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_theme_support('menus');
-    register_nav_menu('header', 'En tête du menu');
-}
-
-function photographe_register_menus() {
+// Ajouter les fonctionnalités du thème
+function photographe_setup() {
+    // Activer la prise en charge des images mises en avant
+    add_theme_support( 'post-thumbnails' );
+    
+    // Ajouter la prise en charge des titres de page
+    add_theme_support( 'title-tag' );
+    
+    // Activer la gestion automatique des menus
     register_nav_menus( array(
-      'primary-menu' => 'header',
+        'primary-menu' => 'Menu principal',
     ) );
-  }
+}
+add_action( 'after_setup_theme', 'photographe_setup' );
 
-add_action('after_setup_theme', 'photographe_supports');
-add_action('after_setup_theme', 'photographe_register_menus');
+// Enqueue les scripts et les styles du thème
+function photographe_enqueue_scripts() {
+    // Enqueue le fichier style.css du thème
+    wp_enqueue_style( 'photographe-style', get_stylesheet_uri() );
+    
+    // Enqueue un script personnalisé (exemple)
+    wp_enqueue_script( 'photographe-custom-script', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), '1.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'photographe_enqueue_scripts' );
--- wp-content/themes/photographe/header.php
+++ wp-content/themes/photographe/header.php
@@ -1,10 +1,21 @@
-<?php
-  // Afficher le menu principal
-  wp_nav_menu( array(
-    'theme_location' => 'primary-menu',
-    'theme_location' => 'header',
-    'container' => 'nav',
-    'container_class' => 'main-menu-container',
-    'menu_class' => 'main-menu',
-  ) );
-?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <nav>
+            <?php
+                // Afficher le menu principal
+                wp_nav_menu( array(
+                    'theme_location' => 'primary-menu',
+                    'container' => 'ul',
+                    'menu_class' => 'main-menu',
+                ) );
+            ?>
+        </nav>
+    </header>
+    <div id="content">
