592533718c075ece63b3f3ffeba61e759ecba542
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 15:05:56 +0200

    test style.css

--- wp-content/themes/PhotographeEvent/header.php
+++ wp-content/themes/PhotographeEvent/header.php
@@ -2,7 +2,7 @@
   // Afficher le menu principal
   wp_nav_menu( array(
     'theme_location' => 'header',
-    'container' => 'false',
+    'container' => false,
     'menu_class' => 'main-menu',
   ) );
 ?>
