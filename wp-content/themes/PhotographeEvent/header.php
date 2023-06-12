1032fc8c6f01c7f576eb70006432938341810623
Sullyvan <75493887+Sullyvan2008@users.noreply.github.com>
2023-06-12 14:53:58 +0200

    changement nom menu

--- wp-content/themes/PhotographeEvent/header.php
+++ wp-content/themes/PhotographeEvent/header.php
@@ -1,7 +1,7 @@
 <?php
   // Afficher le menu principal
   wp_nav_menu( array(
-    'theme_location' => 'primary-menu',
+    'theme_location' => 'header',
     'container' => 'nav',
     'container_class' => 'main-menu-container',
     'menu_class' => 'main-menu',
